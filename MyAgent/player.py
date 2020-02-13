d80b8c2f5fca7872f46609a37a7922859f017a34
Oskars <dervinis@inbox.lv>
2020-02-13 21:14:24 +0000

    234shapesdone

--- MyAgent/player.py
+++ MyAgent/player.py
@@ -24,16 +24,20 @@ class Player(GomokuAgent):
 def readBoard(self, board, ID):
     BOARD_SIZE = board.shape[0]
     
-    figs3VH, figs4VH = boardValRow(self, board, ID)
-    figs3Diag, figs4Diag = boardValDiag(self, board, ID)
+    figs2VH, figs3VH, figs4VH = boardValRow(self, board, ID)#GET figures of specified player horizonta and vertical
+    figs2Diag, figs3Diag, figs4Diag = boardValDiag(self, board, ID)#DIagonal
+    
+    figures2 = figs2VH+figs2Diag#combination of vert and horizontal fig, separated by its length
     figures3 = figs3VH+figs3Diag
     figures4 = figs4VH+figs4Diag
-    
+
+    #print("Figures2: ", figures2)
     #print("Figures3: ", figures3)
     #print("Figures4: ", figures4)
-    return figures3, figures4
+    return figures2, figures3, figures4
 
 def boardValRow(self, board, ID):
+    figures2 = []
     figures3 = []
     figures4 = []
     isRotated = False
@@ -41,38 +45,50 @@ def boardValRow(self, board, ID):
     for times in range (2):
         
         for r in range(BOARD_SIZE):
-            for c in range(BOARD_SIZE - self.X_IN_A_LINE + 3):#last "starting point" is 8, to detet 3 in a row
+            jumpVal = 0#value to jump through desk if last figure was found
+            for c in range(BOARD_SIZE - self.X_IN_A_LINE + 4):#last "starting point" is 8, to detet 3 in a row
+                
                 scores = 0
+                figure = ((-1,-1), (-1,-1))
                 for i in range(self.X_IN_A_LINE - 1):# -1 for detecting up to 4 
                     #print(r,c,i, "SSS",r,c+i)
-                    if c+i < BOARD_SIZE: #protects against out of bounds if last positions in the row
+                    if c + i + jumpVal < BOARD_SIZE: #protects against out of bounds if last positions in the row
                         
-                        if board[r,c+i] == ID:
+                        if board[r,c + i + jumpVal] == ID:
 
                             scores+=1
+
                             #print(r,c+i)
-                            if scores == 4 or scores == 3:
+                            if scores == 4 or scores == 3 or scores == 2:
                                 if isRotated:#VERTICAL
-                                    row = c
+                                    row = c + jumpVal
                                     col = 10-r
-                                    fig = ((row,col), (row + scores - 1,col))
+                                    figure = ((row,col), (row + scores - 1,col))
 
                                 else: #HORIZONTAL
 
                                     row = r
-                                    col = c
+                                    col = c + jumpVal
 
-                                    fig = ((row,col), (row,col + scores - 1))
+                                    figure = ((row,col), (row,col + scores - 1))
 
-                                if scores == 4: 
-                                    figures3.pop()
-                                    figures4.append(fig)
-                                else:
-                                    figures3.append(fig)
+                                
                                 #FIGURA MOZET BITJ ODNA V DRUGOJ
                         else:
                             break
-                        
+                
+
+                if scores == 4: 
+                    figures4.append(figure)
+                elif scores == 3:
+                    figures3.append(figure)
+                elif scores == 2:
+                    figures2.append(figure)
+                
+                if scores > 1:
+                    jumpVal = jumpVal + scores - 1
+                 
+                       
         if not isRotated:
             board = np.rot90(board)
             isRotated = True
@@ -80,46 +96,56 @@ def boardValRow(self, board, ID):
             board = np.rot90(board,3)
             isRotated = False
             
-    return figures3, figures4  
+    return figures2, figures3, figures4  
                     
 def boardValDiag(self, board, ID):
+    figures2 = []
     figures3 = []
     figures4 = []
     
     isRotated = False
     BOARD_SIZE = board.shape[0]
     for times in range (2):
-        
+        blackList = [] #to avoid repeated figures, contains the cells which we dont eed to visit
         for r in range(BOARD_SIZE - self.X_IN_A_LINE + 3):
             for c in range(BOARD_SIZE - self.X_IN_A_LINE + 3):
-                scores = 0
-                for i in range(self.X_IN_A_LINE - 1):
-                    if c+i < BOARD_SIZE and r+i < BOARD_SIZE:
-                        
-                        if board[r + i,c + i] == ID:
-                            scores+=1
-                            if scores == 4 or scores == 3:
-                                if isRotated:#VERTICAL
-                                    row = c
-                                    col = 10-r
-                                    fig = ((row,col), (row + scores - 1, col - scores + 1))
+                if not (r,c) in blackList: #if the point is not part of the figure
+                    figure = (-1,-1)
+                    scores = 0
+                    for i in range(self.X_IN_A_LINE - 1):
+                        if c+i < BOARD_SIZE and r+i < BOARD_SIZE:
+                            
+                            if board[r + i,c + i] == ID:
+                                scores+=1
+                                if scores == 4 or scores == 3 or scores == 2:
+                                    if isRotated:#VERTICAL
+                                        row = c
+                                        col = 10-r
+                                        figure = ((row,col), (row + scores - 1, col - scores + 1))
 
-                                else: #HORIZONTAL
+                                    else: #HORIZONTAL
 
-                                    row = r
-                                    col = c
+                                        row = r
+                                        col = c
 
-                                    fig = ((row,col), (row + scores - 1,col + scores - 1))
+                                        figure = ((row,col), (row + scores - 1,col + scores - 1))
 
-                                if scores == 4: 
-                                    figures3.pop()
-                                    figures4.append(fig)
-                                else:
-                                    figures3.append(fig)#FIGURA MOZET BITJ ODNA V DRUGOJ
-                                #print(fig, isRotated)
-                        else:
-                            break
-                        
+                                    
+                            else:
+                                break
+
+                    if scores == 4:
+                        figures4.append(figure)
+                        blackList.append((r+1, c+1))
+                        blackList.append((r+2, c+2))
+                        blackList.append((r+3, c+3))
+                    elif scores == 3:
+                        figures3.append(figure)
+                        blackList.append((r+1, c+1))
+                        blackList.append((r+2, c+2))
+                    elif scores == 2:
+                        figures2.append(figure)
+                        blackList.append((r+1, c+1))        
         if not isRotated:
             board = np.rot90(board)
             isRotated = True
@@ -127,15 +153,15 @@ def boardValDiag(self, board, ID):
             board = np.rot90(board,3)
             isRotated = False
             
-    return figures3, figures4   
+    return figures2,figures3, figures4   
 
 def determineMove(self, board):
     nextMoveCoords = (-1,-1)
     suggestedMoves = []
-    ally3, ally4 = readBoard(self, board, self.ID)
-    enemy3, enemy4 = readBoard(self, board, -self.ID)
-    print("Ally:",ally3, ally4)
-    print("Enemy:",enemy3, enemy4)
+    ally2, ally3, ally4 = readBoard(self, board, self.ID)
+    enemy2, enemy3, enemy4 = readBoard(self, board, -self.ID)
+    print("Ally:",ally2, ally3, ally4)
+    print("Enemy:",enemy2, enemy3, enemy4)
     
     if ally4:#DETERMINE LAST MOVE TO WIN IF POSSIBLE
         nextMoveCoords = check4InLine(board, ally4)
@@ -153,47 +179,49 @@ def check4InLine(board, fig4):
     nextMoveCoords = (-1,-1)
     for shape in fig4:
             
-            possibleLocs = getMovesOnEdgesForFig(board, shape)
+            possibleLocs = getMovesOnEdgesForFig(board, shape, 1, 0)
                          
             if possibleLocs:
                 nextMoveCoords = possibleLocs[0]
     return nextMoveCoords   
 
-def getMovesOnEdgesForFig(board, figure):
+def getMovesOnEdgesForFig(board, figure, dist, id):
     possibleLocs = []
     y,x = figure[0]
     y1, x1 = figure[1]
     if x == x1:#if vertical
-        if legalMove(board,(y-1, x)):
-            possibleLocs.append((y - 1, x))
-        if legalMove(board,(y1+1, x)):
-            possibleLocs.append((y1 + 1, x))
+        if isBeneficial(board,(y-dist, x), id):
+            possibleLocs.append((y - dist, x))
+        if isBeneficial(board,(y1+dist, x), id):
+            possibleLocs.append((y1 + dist, x))
                     
                     
     elif y == y1:#if horizontal
-        if legalMove(board,(y, x-1)):
-            possibleLocs.append((y, x - 1))
-        if legalMove(board,(y1, x1+1)):
-            possibleLocs.append((y1, x1 + 1))
+        if isBeneficial(board,(y, x-dist), id):
+            possibleLocs.append((y, x - dist))
+        if isBeneficial(board,(y1, x1+dist), id):
+            possibleLocs.append((y1, x1 + dist))
                     
     elif abs(x1-x) == abs(y1-y):#IF DIAGONAL
         if x<x1 and y<y1:#left up to down bot
-            if legalMove(board,(y - 1, x - 1)):
-                possibleLocs.append((y - 1, x - 1))
-            if legalMove(board,(y1 + 1, x1 + 1)):
-                possibleLocs.append((y1 + 1, x1 + 1))
+            if isBeneficial(board,(y - dist, x - dist), id):
+                possibleLocs.append((y - dist, x - dist))
+            if isBeneficial(board,(y1 + dist, x1 + dist), id):
+                possibleLocs.append((y1 + dist, x1 + dist))
         else:#right up to left bot
-            if legalMove(board,(y - 1, x + 1)):
-                possibleLocs.append((y - 1, x + 1))
-            if legalMove(board,(y1 + 1, x1 - 1)):
-                possibleLocs.append((y1 + 1, x1 - 1))
+            if isBeneficial(board,(y - dist, x + dist), id):
+                possibleLocs.append((y - dist, x + dist))
+            if isBeneficial(board,(y1 + dist, x1 - dist), id):
+                possibleLocs.append((y1 + dist, x1 - dist))
     return possibleLocs                    
 
 def checkFigs3(board, fig3):
     suggestedMoves = []
     allPossibleLocs = []
     for fig in fig3:
-        allPossibleLocs.append(getMovesOnEdgesForFig(board, fig))
+        newPossibleLocs = getMovesOnEdgesForFig(board, fig, 1, 0)
+        if newPossibleLocs:
+            allPossibleLocs.append(getMovesOnEdgesForFig(board, fig, 1, 0))
 
     print("POSSIBLE MOVES",allPossibleLocs)                    
     if suggestedMoves:
@@ -207,3 +235,13 @@ def isDecidedMove(coords):
         return False
     else:
         return True    
+
+def isBeneficial(board, moveLoc, id):
+    BOARD_SIZE = board.shape[0]
+    if moveLoc[0] < 0 or moveLoc[0] >= BOARD_SIZE or \
+       moveLoc[1] < 0 or moveLoc[1] >= BOARD_SIZE: 
+        return False
+
+    if board[moveLoc] == id:
+        return True
+    return False
